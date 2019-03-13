61031938b14aced5d22e1c46ad52bcda283cc10e
Lluis <llmas1996@mail.com>
2019-03-13 14:51:36 +0100

    Act 3 finished

--- Act3/app/src/main/java/com/example/hellomaps/MapsActivity.java
+++ Act3/app/src/main/java/com/example/hellomaps/MapsActivity.java
@@ -2,26 +2,54 @@ package com.example.hellomaps;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    public static final CameraPosition ROMANIA  =
+            new CameraPosition.Builder().target(new LatLng(44.439663, 26.096306))
+                    .zoom(15.5f)
+                    .bearing(0)
+                    .tilt(25)
+                    .build();
+
+    public static final CameraPosition BONDI =
+            new CameraPosition.Builder().target(new LatLng(-33.891614, 151.276417))
+                    .zoom(15.5f)
+                    .bearing(300)
+                    .tilt(50)
+                    .build();
+
     private GoogleMap mMap;
+
+    private CompoundButton mAnimateToggle;
+    private CompoundButton mCustomDurationToggle;
+    private SeekBar mCustomDurationBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        mAnimateToggle = (CompoundButton) findViewById(R.id.animate);
+        mCustomDurationToggle = (CompoundButton) findViewById(R.id.duration_toggle);
+        mCustomDurationBar = (SeekBar) findViewById(R.id.duration_bar);
     }
 
 
@@ -42,5 +70,62 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+    }
+
+    public void onToggleCustomDuration(View view) {updateEnabledState();}
+    public void onToggleAnimate(View view) {
+        updateEnabledState();
+    }
+
+    private void updateEnabledState() {
+        mCustomDurationToggle.setEnabled(mAnimateToggle.isChecked());
+        mCustomDurationBar.setEnabled(mAnimateToggle.isChecked() && mCustomDurationToggle.isChecked());
+    }
+
+    private boolean checkReady() {
+        if (mMap == null) {
+            Toast.makeText(this, R.string.map_not_ready, Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    public void onGoToLocation(View view){
+        if (!checkReady())
+            return;
+
+        CameraPosition  position = (view.getId() == R.id.sydney) ? ROMANIA : BONDI;
+        changeCamera(CameraUpdateFactory.newCameraPosition(position), new GoogleMap.CancelableCallback() {
+            @Override
+            public void onFinish() {
+                Toast.makeText(getBaseContext(), "Animation complete", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(getBaseContext(), "Animation canceled", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void changeCamera(CameraUpdate update, GoogleMap.CancelableCallback callback) {
+        if (mAnimateToggle.isChecked()) {
+            if (mCustomDurationToggle.isChecked()) {
+                int duration = mCustomDurationBar.getProgress();
+                // The duration must be strictly positive so we make it at least 1.
+                mMap.animateCamera(update, Math.max(duration, 1), callback);
+            } else {
+                mMap.animateCamera(update, callback);
+            }
+        } else {
+            mMap.moveCamera(update);
+        }
+    }
+
+    public void onStopAnimation(View view) {
+        if (checkReady())
+            mMap.stopAnimation(); // Google
     }
 }
